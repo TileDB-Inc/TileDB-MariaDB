18111e63ddd7e814a3daa81d27f2fff9a5981952
Seth Shelnutt <seth@tiledb.io>
2019-10-02 17:33:01 -0400

    WIP: improve and combine build script

---
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -v -e -x
+
+original_dir=$PWD
+export MARIADB_VERSION="mariadb-10.4.8"
+mkdir tmp
+shopt -s extglob
+mv !(tmp) tmp # Move everything but tmp
+# Download mariadb, this has a habit of failing so let's do it first
+wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARIADB_VERSION}.tar.gz
+
+# Install tiledb using 1.6 release
+mkdir build_deps && cd build_deps \
+&& git clone https://github.com/TileDB-Inc/TileDB.git -b 1.6.3 && cd TileDB \
+&& export deps_args="" \
+&& export bootstrap_args="--enable=verbose,static-tiledb,s3,serialization" \
+&& mkdir -p build && cd build
+
+# Configure and build TileDB
+../bootstrap $bootstrap_args \
+&& make -j4 \
+&& make -C tiledb install \
+&& cd $original_dir
+
+tar xf ${MARIADB_VERSION}.tar.gz \
+&& mv tmp ${MARIADB_VERSION}/storage/mytile \
+&& cd ${MARIADB_VERSION} \
+&& mkdir builddir \
+&& cd builddir \
+&& cmake -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug .. \
+&& make -j$(nproc) \
+&& if ! ./mysql-test/mysql-test-run.pl  --suite=mytile --debug; then cat ./mysql-test/var/log/mysqld.1.err && false; fi;

bb14fc61d689ab8d8ed88b8674a285e42bcbb984
Seth Shelnutt <seth@tiledb.io>
2019-10-03 08:54:14 -0400

    Enable debug builds for CI

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -14,7 +14,7 @@ wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARI
 mkdir build_deps && cd build_deps \
 && git clone https://github.com/TileDB-Inc/TileDB.git -b 1.6.3 && cd TileDB \
 && export deps_args="" \
-&& export bootstrap_args="--enable=verbose,static-tiledb,s3,serialization" \
+&& export bootstrap_args="--enable=verbose,static-tiledb,s3,serialization,debug" \
 && mkdir -p build && cd build
 
 # Configure and build TileDB
@@ -28,6 +28,6 @@ tar xf ${MARIADB_VERSION}.tar.gz \
 && cd ${MARIADB_VERSION} \
 && mkdir builddir \
 && cd builddir \
-&& cmake -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug .. \
+&& cmake -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug -SWITH_DEBUG=1 .. \
 && make -j$(nproc) \
-&& if ! ./mysql-test/mysql-test-run.pl  --suite=mytile --debug; then cat ./mysql-test/var/log/mysqld.1.err && false; fi;
+&& if ! ./mysql-test/mysql-test-run.pl --suite=mytile --debug; then cat ./mysql-test/var/log/mysqld.1.err && false; fi;

2a2b2f329026974cfa50e3bf51a91adc58150fc9
Seth Shelnutt <seth@tiledb.io>
2019-10-07 12:52:59 -0400

    Switch ci to use git for mariadb to fix MDEV-20767

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -8,7 +8,8 @@ mkdir tmp
 shopt -s extglob
 mv !(tmp) tmp # Move everything but tmp
 # Download mariadb, this has a habit of failing so let's do it first
-wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARIADB_VERSION}.tar.gz
+#wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARIADB_VERSION}.tar.gz
+git clone https://github.com/Shelnutt2/server.git -b MDEV-20767 ${MARIADB_VERSION}
 
 # Install tiledb using 1.6 release
 mkdir build_deps && cd build_deps \
@@ -23,8 +24,8 @@ mkdir build_deps && cd build_deps \
 && make -C tiledb install \
 && cd $original_dir
 
-tar xf ${MARIADB_VERSION}.tar.gz \
-&& mv tmp ${MARIADB_VERSION}/storage/mytile \
+#tar xf ${MARIADB_VERSION}.tar.gz \
+mv tmp ${MARIADB_VERSION}/storage/mytile \
 && cd ${MARIADB_VERSION} \
 && mkdir builddir \
 && cd builddir \

b319eaf3eea0dfaf49b64d7a3cd983e32d7190f7
Seth Shelnutt <seth@tiledb.io>
2019-10-08 08:29:33 -0400

    Switch to system install of tiledb for cmake source build

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -9,19 +9,17 @@ shopt -s extglob
 mv !(tmp) tmp # Move everything but tmp
 # Download mariadb, this has a habit of failing so let's do it first
 #wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARIADB_VERSION}.tar.gz
-git clone https://github.com/Shelnutt2/server.git -b MDEV-20767 ${MARIADB_VERSION}
+git clone https://github.com/Shelnutt2/server.git -b MDEV-20767-10.4 ${MARIADB_VERSION}
 
 # Install tiledb using 1.6 release
 mkdir build_deps && cd build_deps \
-&& git clone https://github.com/TileDB-Inc/TileDB.git -b ttd/backport-schema-h && cd TileDB \
-&& export deps_args="" \
-&& export bootstrap_args="--enable=verbose,static-tiledb,s3,serialization,debug" \
+&& git clone https://github.com/TileDB-Inc/TileDB.git -b 1.6.3 && cd TileDB \
 && mkdir -p build && cd build
 
 # Configure and build TileDB
-../bootstrap $bootstrap_args \
+cmake -DTILEDB_VERBOSE=ON -DTILEDB_S3=ON -DTILEDB_SERIALIZATION=ON -DCMAKE_BUILD_TYPE=Debug .. \
 && make -j4 \
-&& make -C tiledb install \
+&& sudo make -C tiledb install \
 && cd $original_dir
 
 #tar xf ${MARIADB_VERSION}.tar.gz \
@@ -29,6 +27,6 @@ mv tmp ${MARIADB_VERSION}/storage/mytile \
 && cd ${MARIADB_VERSION} \
 && mkdir builddir \
 && cd builddir \
-&& cmake -DCMAKE_PREFIX_PATH=../build_deps/TileDB/dist -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug -SWITH_DEBUG=1 .. \
+&& cmake -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug -SWITH_DEBUG=1 .. \
 && make -j$(nproc) \
 && if ! ./mysql-test/mysql-test-run.pl --suite=mytile --debug; then cat ./mysql-test/var/log/mysqld.1.err && false; fi;

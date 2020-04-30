3dcaa0cdd876ae6f394ba02075d3535ebf433d5a
Seth Shelnutt <Shelnutt2@gmail.com>
2020-04-30 14:12:10 -0400

    Update to TileDB 2.0.0

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -11,9 +11,9 @@ mv !(tmp) tmp # Move everything but tmp
 wget https://downloads.mariadb.org/interstitial/${MARIADB_VERSION}/source/${MARIADB_VERSION}.tar.gz
 tar xf ${MARIADB_VERSION}.tar.gz
 
-# Install tiledb using 2.0.0-rc4 release
+# Install tiledb using 2.0.0 release
 mkdir build_deps && cd build_deps \
-&& git clone https://github.com/TileDB-Inc/TileDB.git -b dev && cd TileDB \
+&& git clone https://github.com/TileDB-Inc/TileDB.git -b 2.0.0 && cd TileDB \
 && mkdir -p build && cd build
 
 # Configure and build TileDB

311bb81a87377a138572fb26cab182cb83781e26
Seth Shelnutt <Shelnutt2@gmail.com>
2020-09-10 23:37:33 -0400

    Update for removal of primary_key_is_clustered
    MariaDB 10.5 now only uses index flags to determine if a key is
    clustered. Since we only report one index (primary or not) we uniformly
    set all indexes to clustered now.
    
    For MariaDB change see: https://github.com/MariaDB/server/commit/37393bea23d10741cf99b59ddcaffd58e9652bb9

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -2461,7 +2461,11 @@ ulong tile::mytile::index_flags(uint idx, uint part, bool all_parts) const {
   DBUG_ENTER("tile::mytile::index_flags");
   DBUG_RETURN(HA_READ_NEXT | HA_READ_PREV | HA_READ_ORDER | HA_READ_RANGE |
               HA_KEYREAD_ONLY | HA_DO_RANGE_FILTER_PUSHDOWN |
+#if MYSQL_VERSION_ID < 100500
               HA_DO_INDEX_COND_PUSHDOWN);
+#else
+              HA_DO_INDEX_COND_PUSHDOWN | HA_CLUSTERED_INDEX);
+#endif
 }
 
 void tile::mytile::open_array_for_reads(THD *thd) {

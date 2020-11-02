d8fc5c2ded75525f8829e25c808aab16dc69b098
erica mann <erica@tiledb.com>
2020-11-02 17:09:42 +0000

    update version to 0.6.2

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -2603,7 +2603,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0061,                     /* version number (0.6.1) */
+    0x0062,                     /* version number (0.6.2) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
     NULL,                                   /* config options */
@@ -2620,9 +2620,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0061,                     /* version number (0.6.1) */
+    0x0062,                     /* version number (0.6.2) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
-    "0.6.1",                                /* string version */
+    "0.6.2",                                /* string version */
     MariaDB_PLUGIN_MATURITY_BETA            /* maturity */
 } maria_declare_plugin_end;

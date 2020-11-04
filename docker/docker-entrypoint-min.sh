92825e334212c79b470059dea0220a0b2f7dd413
erica mann <erica@tiledb.com>
2020-11-04 13:09:25 +0000

    remove tile user, set bind address to allow all

--- docker/docker-entrypoint-min.sh
+++ docker/docker-entrypoint-min.sh
@@ -2,13 +2,4 @@
 set -eo pipefail
 shopt -s nullglob
 
-/opt/server/bin/mariadbd --datadir=/var/lib/mysql
-/opt/server/bin/mariadb -e "CREATE USER 'tile'@'localhost' IDENTIFIED BY 'tile';"
-/opt/server/bin/mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'tile'@'localhost' WITH GRANT OPTION"
-/opt/server/bin/mariadb -e "CREATE USER 'tile'@'%' IDENTIFIED BY 'tile';"
-/opt/server/bin/mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'tile'@'%' WITH GRANT OPTION"
-/opt/server/bin/mariadb -e "FLUSH PRIVILEGES"
-killall mariadbd
-/opt/server/bin/mariadbd --datadir=/var/lib/mysql
-
 exec "$@"

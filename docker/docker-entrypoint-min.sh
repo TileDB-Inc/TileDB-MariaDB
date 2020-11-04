e718446a3ac9af8e40823990d39a7f24f1cd1802
erica mann <erica@tiledb.com>
2020-11-04 15:59:55 +0000

    working correctly with startup wait and user privileges in entry point

--- docker/docker-entrypoint-min.sh
+++ docker/docker-entrypoint-min.sh
@@ -2,4 +2,19 @@
 set -eo pipefail
 shopt -s nullglob
 
+/opt/server/bin/mariadbd --datadir=/var/lib/mysql &
+
+for i in {30..0}; do
+  if /opt/server/bin/mariadb -u root -e "SELECT 1;" &> /dev/null; then
+    break
+  fi
+  echo 'MariaDB starting.....'
+  sleep 0.1
+done
+
+/opt/server/bin/mariadb -u root -e "CREATE USER 'root'@'%';"
+/opt/server/bin/mariadb -u root -e "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+/opt/server/bin/mariadb -u root -e "FLUSH PRIVILEGES;"
+sleep infinity
+
 exec "$@"

a71994c98c204a3411a1573d911b85b4a21e8fbc
Seth Shelnutt <Shelnutt2@gmail.com>
2020-12-03 22:54:05 -0500

    Update to mariadb-10.5.9

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -3,7 +3,7 @@
 set -v -e -x
 
 original_dir=$PWD
-export MARIADB_VERSION="mariadb-10.5.5"
+export MARIADB_VERSION=${MARIADB_VERSION:="mariadb-10.5.9"}
 mkdir tmp
 shopt -s extglob
 mv !(tmp) tmp # Move everything but tmp

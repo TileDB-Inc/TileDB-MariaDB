dee7eaaf22a8df7763a121c073357f732f06f91c
Seth Shelnutt <Shelnutt2@gmail.com>
2021-01-13 07:13:29 -0500

    Bump version to 0.7

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -2665,7 +2665,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0066,                     /* version number (0.6.6) */
+    0x0070,                     /* version number (0.6.6) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
     NULL,                                   /* config options */
@@ -2682,9 +2682,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0066,                     /* version number (0.6.6) */
+    0x0070,                     /* version number (0.6.6) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
-    "0.6.6",                                /* string version */
+    "0.7.0",                                /* string version */
     MariaDB_PLUGIN_MATURITY_BETA            /* maturity */
 } maria_declare_plugin_end;

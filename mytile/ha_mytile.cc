4a3f44aaf78c8abdaa7ea46a415ffa5e13d475cd
Seth Shelnutt <Shelnutt2@gmail.com>
2022-04-13 14:57:11 -0400

    Update to TileDB 2.8.0

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -3347,7 +3347,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0130,                     /* version number (0.13.0) */
+    0x0140,                     /* version number (0.14.0) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
     NULL,                                   /* config options */
@@ -3364,9 +3364,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0126,                     /* version number (0.12.1) */
+    0x0140,                     /* version number (0.14.0) */
     NULL,                       /* status variables */
     tile::sysvars::mytile_system_variables, /* system variables */
-    "0.13.0",                               /* string version */
+    "0.14.0",                               /* string version */
     MariaDB_PLUGIN_MATURITY_BETA            /* maturity */
 } maria_declare_plugin_end;
--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -14,14 +14,14 @@ git clone --recurse-submodules https://github.com/MariaDB/server.git -b ${MARIAD
 if [[ -z ${SUPERBUILD+x} || "${SUPERBUILD}" == "OFF" ]]; then
 
   if [[ "$AGENT_OS" == "Linux" ]]; then
-    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.7.2/tiledb-linux-x86_64-2.7.2-c0362d4.tar.gz \
+    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.8.0/tiledb-linux-x86_64-2.8.0-f8efd39.tar.gz \
     && sudo tar -C /usr/local -xvf tiledb.tar.gz
   elif [[ "$AGENT_OS" == "Darwin" ]]; then
-    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.7.2/tiledb-macos-x86_64-2.7.2-c0362d4.tar.gz \
+    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.8.0/tiledb-macos-x86_64-2.8.0-f8efd39.tar.gz \
     && sudo tar -C /usr/local -xvf tiledb.tar.gz
   else
     mkdir build_deps && cd build_deps \
-    && git clone https://github.com/TileDB-Inc/TileDB.git -b 2.7.2 && cd TileDB \
+    && git clone https://github.com/TileDB-Inc/TileDB.git -b 2.8.0 && cd TileDB \
     && mkdir -p build && cd build
 
      # Configure and build TileDB

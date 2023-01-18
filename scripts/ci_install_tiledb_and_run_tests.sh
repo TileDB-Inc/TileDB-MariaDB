9b0910ac3938d565d6b28c7af8c0cc8bffd35847
Seth Shelnutt <seth@tiledb.io>
2023-01-18 15:22:29 -0500

    Update to TileDB 2.14.0

--- scripts/ci_install_tiledb_and_run_tests.sh
+++ scripts/ci_install_tiledb_and_run_tests.sh
@@ -16,14 +16,14 @@ TILEDB_FORCE_ALL_DEPS=${TILEDB_FORCE_ALL_DEPS:="OFF"}
 if [[ -z ${SUPERBUILD+x} || "${SUPERBUILD}" == "OFF" ]]; then
 
   if [[ "$AGENT_OS" == "Linux" ]]; then
-    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0-rc0/tiledb-linux-x86_64-2.14.0-rc0-58fa524.tar.gz \
+    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-linux-x86_64-2.14.0-27eed08.tar.gz \
     && sudo tar -C /usr/local -xvf tiledb.tar.gz
   elif [[ "$AGENT_OS" == "Darwin" ]]; then
-    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0-rc0/tiledb-macos-x86_64-2.14.0-rc0-58fa524.tar.gz \
+    curl -L -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.14.0/tiledb-macos-x86_64-2.14.0-27eed08.tar.gz \
     && sudo tar -C /usr/local -xvf tiledb.tar.gz
   else
     mkdir build_deps && cd build_deps \
-    && git clone https://github.com/TileDB-Inc/TileDB.git -b 2.14.0-rc0 && cd TileDB \
+    && git clone https://github.com/TileDB-Inc/TileDB.git -b 2.14.0 && cd TileDB \
     && mkdir -p build && cd build
 
      # Configure and build TileDB

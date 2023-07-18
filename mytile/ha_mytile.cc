e6e02ee1b11baa052af123bc05a8e39fccb95d2b
dimitrisstaratzis <jstaratzi@icloud.com>
2023-07-18 13:50:11 +0300

    MyTile version bump to 0.23.1

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -3704,7 +3704,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0230,                     /* version number (0.23.0) */
+    0x0231,                     /* version number (0.23.1) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
     NULL,                                      /* config options */
@@ -3721,9 +3721,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0230,                     /* version number (0.23.0) */
+    0x0231,                     /* version number (0.23.1) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
-    "0.23.0",                                  /* string version */
+    "0.23.1",                                  /* string version */
     MariaDB_PLUGIN_MATURITY_BETA               /* maturity */
 } maria_declare_plugin_end;

cacc61994e2b9b9a37387eea2bfc14220c375cc4
Dimitris Staratzis <33267511+DimitrisStaratzis@users.noreply.github.com>
2023-11-27 17:26:11 +0200

    update to TileDB-2.18.0 (#289)
    * update to TileDB-2.18.0
    
    * fix linux superbuild
    
    * update to CXX standard 20
    
    * update to Ubuntu 22.04

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -3755,7 +3755,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0255,                     /* version number (0.25.5) */
+    0x0260,                     /* version number (0.26.0) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
     NULL,                                      /* config options */
@@ -3772,9 +3772,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0255,                     /* version number (0.25.5) */
+    0x0260,                     /* version number (0.26.0) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
-    "0.25.5",                                  /* string version */
+    "0.26.0",                                  /* string version */
     MariaDB_PLUGIN_MATURITY_BETA               /* maturity */
 } maria_declare_plugin_end;

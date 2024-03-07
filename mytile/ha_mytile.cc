f02d84afeed09054c325a521703d8b528dfc627e
Dimitris Staratzis <33267511+DimitrisStaratzis@users.noreply.github.com>
2024-03-07 11:23:12 +0200

    update to TileDB 2.20.1

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -3754,7 +3754,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0271,                     /* version number (0.27.1) */
+    0x0280,                     /* version number (0.28.0) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
     NULL,                                      /* config options */
@@ -3771,9 +3771,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0271,                     /* version number (0.27.1) */
+    0x0280,                     /* version number (0.28.0) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
-    "0.27.1",                                  /* string version */
+    "0.28.0",                                  /* string version */
     MariaDB_PLUGIN_MATURITY_BETA               /* maturity */
 } maria_declare_plugin_end;

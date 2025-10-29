31469731694b6c5e873bfbaf5d3644e67230eab5
Isaiah Norton <isaiah@tiledb.com>
2025-10-29 14:26:06 -0400

    Bump versions for MyTile 0.37 / TileDB Arrays 2.29

--- mytile/ha_mytile.cc
+++ mytile/ha_mytile.cc
@@ -4378,7 +4378,7 @@ mysql_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0361,                     /* version number (0.36.1) */
+    0x0370,                     /* version number (0.36.1) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
     NULL,                                      /* config options */
@@ -4395,9 +4395,9 @@ maria_declare_plugin(mytile){
     PLUGIN_LICENSE_PROPRIETARY, /* the plugin license (PLUGIN_LICENSE_XXX) */
     mytile_init_func,           /* Plugin Init */
     NULL,                       /* Plugin Deinit */
-    0x0361,                     /* version number (0.36.1) */
+    0x0370,                     /* version number (0.36.1) */
     tile::statusvars::mytile_status_variables, /* status variables */
     tile::sysvars::mytile_system_variables,    /* system variables */
-    "0.36.1",                                  /* string version */
+    "0.37.0",                                  /* string version */
     MariaDB_PLUGIN_MATURITY_BETA               /* maturity */
 } maria_declare_plugin_end;
